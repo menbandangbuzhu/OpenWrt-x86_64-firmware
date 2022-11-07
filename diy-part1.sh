0dbbfba8652c130e9b9e43bf61764afde4d7d337
menbandangbuzhu <113989952+menbandangbuzhu@users.noreply.github.com>
2022-11-07 08:23:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,7 +8,7 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+#git clone https://github.com/immortalwrt-collections/openwrt-filebrowser.git
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
